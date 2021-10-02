965e8c3c1c588d16a85d1d8e0c8ba155815f140c
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-02 13:35:38 -0300

    Create a new DTO file

---
+++ PlayGame/Play.Catalog/src/Play.Catalog.Service/Dtos.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Play.Catalog.Service.Dtos
+{
+    public record ItemDto(Guid Id, string name, string Description, decimal Price, DateTimeOffset CreatedDate);
+
+    public record CreateItemDto(string Name, string Description, decimal Price);
+
+    public record UpdateItemDto(string Name, string Description, decimal Price);
+}
