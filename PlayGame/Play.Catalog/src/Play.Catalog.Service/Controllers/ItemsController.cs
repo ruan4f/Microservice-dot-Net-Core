a568ef7a5e60621ae4aeacd637a65c1b541d7821
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-03 20:38:55 -0300

    Update ItemsController.cs

--- PlayGame/Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ PlayGame/Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Service.Dtos;
 
@@ -7,8 +8,8 @@ namespace Play.Catalog.Service.Controllers
 {
     // https://localhost:5001/items
     [ApiController]
-    [Route("items")]   
-    public class ItemsController: ControllerBase
+    [Route("items")]
+    public class ItemsController : ControllerBase
     {
         private static readonly List<ItemDto> items = new()
         {
@@ -16,5 +17,19 @@ namespace Play.Catalog.Service.Controllers
             new ItemDto(Guid.NewGuid(), "Antidote", "Cure posion", 7, DateTimeOffset.UtcNow),
             new ItemDto(Guid.NewGuid(), "Bronze sword", "Dela a small amount of damege", 20, DateTimeOffset.UtcNow)
         };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        // GET /items/{id}/
+        [HttpGet("{id}")]
+        public ItemDto GetById(Guid id)
+        {
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+            return item;
+        }
     }
 }
