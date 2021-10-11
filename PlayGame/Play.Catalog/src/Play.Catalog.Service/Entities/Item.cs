28da4eb61d846f9fdd69cb9151876d3a0571eedf
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-10 22:15:50 -0300

    Create Item.cs

---
+++ PlayGame/Play.Catalog/src/Play.Catalog.Service/Entities/Item.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Play.Catalog.Service.Entities
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
