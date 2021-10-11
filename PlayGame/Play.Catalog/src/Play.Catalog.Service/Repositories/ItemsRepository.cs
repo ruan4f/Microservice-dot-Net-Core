920a8bb8480da7528ee55ebd40086446b64426b0
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-10 23:32:51 -0300

    Update ItemsRepository.cs

--- PlayGame/Play.Catalog/src/Play.Catalog.Service/Repositories/ItemsRepository.cs
+++ PlayGame/Play.Catalog/src/Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -1,7 +1,20 @@
+using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
+
 namespace Play.Catalog.Service.Repositories
 {
     public class ItemsRepository
     {
-        
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> dbCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+        public ItemsRepository()
+        {
+            var mongoCliente = new MongoClient("mongodb://localhost:27017");
+
+            
+        }
+
     }
 }
