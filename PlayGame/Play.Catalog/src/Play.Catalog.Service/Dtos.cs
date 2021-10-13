2f2acd229237b34e847f8d7db1dd93fe75165697
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-12 23:27:41 -0300

    Update

--- PlayGame/Play.Catalog/src/Play.Catalog.Service/Dtos.cs
+++ PlayGame/Play.Catalog/src/Play.Catalog.Service/Dtos.cs
@@ -4,8 +4,6 @@ using System.ComponentModel.DataAnnotations;
 namespace Play.Catalog.Service.Dtos
 {
     public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
-
     public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
-
     public record UpdateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 }
--- PlayGame/Play.Catalog/src/Play.Catalog.Service/Settings/MongoDbSettings.cs
+++ PlayGame/Play.Catalog/src/Play.Catalog.Service/Settings/MongoDbSettings.cs
@@ -3,9 +3,7 @@ namespace Play.Catalog.Service.Settings
     public class MongoDbSettings
     {
         public string Host { get; init; }
-
         public int Port { get;init;}
-
         public string ConnectionString => $"mongodb://{Host}:{Port}";
     }
 }
