7b50e67f536708ae06b3e2355bf9fee75f82855d
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-13 23:57:21 -0300

    Create the base of repository

---
+++ PlayGame/Play.Common/src/Play.Common/IRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Play.Common;
+
+namespace Play.Common
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task CreateAsync(T entity);
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task<T> GetAsync(Guid id);
+        Task RemoveAsync(Guid id);
+        Task UpdateAsync(T entity);
+    }
+}
