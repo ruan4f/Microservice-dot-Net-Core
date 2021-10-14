20340f024bebcc554e70fad97483d6d31cfca512
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-13 23:33:00 -0300

    Update Startup.cs

--- PlayGame/Play.Catalog/src/Play.Catalog.Service/Startup.cs
+++ PlayGame/Play.Catalog/src/Play.Catalog.Service/Startup.cs
@@ -15,6 +15,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
 using Play.Catalog.Service.Repositories;
 using Play.Catalog.Service.Settings;
 
@@ -45,7 +46,10 @@ namespace Play.Catalog.Service
                 return mongoClient.GetDatabase(serviceSettings.ServiceName);
             });
 
-            services.AddSingleton<IItemsRepository, ItemsRepository>();
+            services.AddSingleton<IRepository<Item>>( serviceProvider => {
+                var database = serviceProvider.GetService<IMongoDatabase>();
+                return new MongoRepository<Item>(database, "items");
+            });
 
             services.AddControllers(options =>
             {
