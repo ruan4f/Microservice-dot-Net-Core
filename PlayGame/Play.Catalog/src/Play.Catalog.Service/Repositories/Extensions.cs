578804e80b66b9043bc9d94ece7acbe6dde91b7c
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-13 23:37:42 -0300

    Create Extensions.cs

---
+++ PlayGame/Play.Catalog/src/Play.Catalog.Service/Repositories/Extensions.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Play.Catalog.Service.Settings;
+
+namespace Play.Catalog.Service.Repositories
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMongo(this IServiceCollection services)
+        {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));            
+
+            services.AddSingleton(serviceProvider => {
+                var configuration = serviceProvider.GetService<IConfiguration>();
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+                return mongoClient.GetDatabase(serviceSettings.ServiceName);
+            });
+
+            return services;
+        }
+    }
+}
