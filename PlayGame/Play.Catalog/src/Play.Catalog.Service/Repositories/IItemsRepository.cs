7d3cc9bdb34418750c0cfb88af8a5eeb66317c0b
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-13 23:25:47 -0300

    Update IItemsRepository.cs

--- PlayGame/Play.Catalog/src/Play.Catalog.Service/Repositories/IItemsRepository.cs
+++ PlayGame/Play.Catalog/src/Play.Catalog.Service/Repositories/IItemsRepository.cs
@@ -5,12 +5,12 @@ using Play.Catalog.Service.Entities;
 
 namespace Play.Catalog.Service.Repositories
 {
-    public interface IItemsRepository
+    public interface IRepository<T> where T : IEntity
     {
-        Task CreateAsync(Item entity);
-        Task<IReadOnlyCollection<Item>> GetAllAsync();
-        Task<Item> GetAsync(Guid id);
+        Task CreateAsync(T entity);
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task<T> GetAsync(Guid id);
         Task RemoveAsync(Guid id);
-        Task UpdateAsync(Item entity);
+        Task UpdateAsync(T entity);
     }
 }
