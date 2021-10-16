bc7f6f267b9c910ce06351216825470682a0072f
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-16 00:28:07 -0300

    Create a Entity InventoryItem

--- PlayGame/Play.Inventory/src/Play.Inventory.Service/Dtos.cs
+++ PlayGame/Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -4,5 +4,5 @@ namespace Play.Inventory.Service.Dtos
 {
     public record GrantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
 
-    public record InventoryItemDto(Guid CatalogID, int Quantity, DateTimeOffset AcquiredDate);
+    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
 }
--- PlayGame/Play.Inventory/src/Play.Inventory.Service/Entities/InventoryItem.cs
+++ PlayGame/Play.Inventory/src/Play.Inventory.Service/Entities/InventoryItem.cs
@@ -6,5 +6,14 @@ namespace Play.Inventory.Service.Entities
     public class InventoryItem : IEntity
     {
         public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid CatalogItemId { get; set; }
+    
+        public int Quantity { get; set; }
+
+        public DateTimeOffset AcquiredDate { get; set; }
+
     }
 }
