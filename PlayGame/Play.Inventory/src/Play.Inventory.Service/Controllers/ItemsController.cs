442371597f7357596f019eba34c9a73e2205d1cd
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-16 17:04:44 -0300

    Create ItemsController.cs

---
+++ PlayGame/Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Play.Common;
+using Play.Inventory.Service.Entities;
+
+namespace Play.Inventory.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IRepository<InventoryItem> itemsRepository;
+    }
+}
