735ca3a7773fa379b0c74abc64f8b1af2dfaffec
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-16 00:18:53 -0300

    Update Program.cs

--- PlayGame/Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ PlayGame/Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Play.Invetory.Service
+namespace Play.Inventory.Service
 {
     public class Program
     {
