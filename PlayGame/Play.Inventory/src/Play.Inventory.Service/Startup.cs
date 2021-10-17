cc9d81e92d2c81a305c41152c632eafaa1189bd0
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-17 17:36:06 -0300

    Adjust in configuration files

--- PlayGame/Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ PlayGame/Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -34,7 +34,7 @@ namespace Play.Inventory.Service
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Invetory.Service", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Inventory.Service", Version = "v1" });
             });
         }
 
@@ -45,7 +45,7 @@ namespace Play.Inventory.Service
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Invetory.Service v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Inventory.Service v1"));
             }
 
             app.UseHttpsRedirection();
