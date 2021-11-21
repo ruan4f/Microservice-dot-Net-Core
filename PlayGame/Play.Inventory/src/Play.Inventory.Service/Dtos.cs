7615980f234fc1c26d15d1900c584450b189c732
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-11-20 23:31:22 -0300

    Update Dtos.cs

--- PlayGame/Play.Inventory/src/Play.Inventory.Service/Dtos.cs
+++ PlayGame/Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -2,7 +2,9 @@ using System;
 
 namespace Play.Inventory.Service.Dtos
 {
-    public record GrantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
+  public record GrantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
 
-    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+  public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+
+  public record CatalogItemDto(Guid Id, string Name, string Description);
 }
