b0b6263417dcc6a018f8068596708dca1a97985e
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-11-20 23:44:30 -0300

    new

--- PlayGame/Play.Inventory/src/Play.Inventory.Service/Clients/CatalogClient.cs
+++ PlayGame/Play.Inventory/src/Play.Inventory.Service/Clients/CatalogClient.cs
@@ -16,7 +16,9 @@ namespace Play.Inventory.Service.Clients
 
     public async Task<IReadOnlyCollection<CatalogItemDto>> GetCatalogItemsAsync()
     {
+      var items = await httpClient.GetFromJsonAsync<IReadOnlyCollection<CatalogItemDto>>("/items");
 
+      return items;
     }
   }
 }
