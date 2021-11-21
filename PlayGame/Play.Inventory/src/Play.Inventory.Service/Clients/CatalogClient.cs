b2801d59cc3d0d6f82b775352dca4dfd0d26f2c7
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-11-20 23:39:19 -0300

    Create CatalogClient.cs

---
+++ PlayGame/Play.Inventory/src/Play.Inventory.Service/Clients/CatalogClient.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Play.Inventory.Service.Dtos;
+
+namespace Play.Inventory.Service.Clients
+{
+  public class CatalogClient
+  {
+    private readonly HttpClient httpClient;
+
+    public CatalogClient(HttpClient httpClient)
+    {
+      this.httpClient = httpClient;
+    }
+
+    public async Task<IReadOnlyCollection<CatalogItemDto>> GetCatalogItemsAsync()
+    {
+
+    }
+  }
+}
